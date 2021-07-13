cc52571737da909b1b4a916180aedf376b405db9
Tom de Bruijn <tom@tomdebruijn.com>
2021-07-13 09:19:43 +0200

    Add v0.3.0 release
    Bump the tap to use v0.3.0 for new installs and update existing
    installs.

--- Formula/lintje.rb
+++ Formula/lintje.rb
@@ -1,13 +1,13 @@
 class Lintje < Formula
   desc "An opinionated linter for Git"
   homepage "https://github.com/tombruijn/lintje"
-  version "0.2.0"
+  version "0.3.0"
   if Hardware::CPU.arm?
-    url "https://github.com/tombruijn/lintje/releases/download/v0.2.0/aarch64-apple-darwin.tar.gz"
-    sha256 "9fcbe2e4dd463a10faed15a0412f5735045a3dacb0a8dc47de0242dd7f813823"
+    url "https://github.com/tombruijn/lintje/releases/download/v0.3.0/aarch64-apple-darwin.tar.gz"
+    sha256 "802f42a339d7993b642131f664bb1253126f151d3bc6183bf31f4d72920f50b0"
   else
-    url "https://github.com/tombruijn/lintje/releases/download/v0.2.0/x86_64-apple-darwin.tar.gz"
-    sha256 "612cc1d75c3a2dc24b59d8506b01c1fdf1f4f1b62523bca9d4c5abc0c7cc4e7d"
+    url "https://github.com/tombruijn/lintje/releases/download/v0.3.0/x86_64-apple-darwin.tar.gz"
+    sha256 "b1274012c44349a6c9aac336ff2aac57c73e58c6cc9025f9357919fdc4d1606a"
   end
   license "MIT"
 
