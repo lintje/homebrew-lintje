dc078391cc0648bdc39b156d88b15ae38bef026f
Tom de Bruijn <tom@tomdebruijn.com>
2022-03-15 22:26:49 +0100

    Add v0.7.0 release
    Bump the tap to use the latest version for new installs and update
    existing installs.

--- Formula/lintje.rb
+++ Formula/lintje.rb
@@ -1,21 +1,20 @@
 class Lintje < Formula
   desc "Opinionated linter for Git"
-  homepage "https://github.com/tombruijn/lintje"
-  version "0.6.1"
+  homepage "https://lintje.dev"
+  version "0.7.0"
   license "MIT"
-  revision 1
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tombruijn/lintje/releases/download/v0.6.1/aarch64-apple-darwin.tar.gz"
-    sha256 "ecd7735f8fd54f59436b791485ad7c3e3d0ed95f451c019844bc7480a8bb34ae"
+    url "https://github.com/tombruijn/lintje/releases/download/v0.7.0/aarch64-apple-darwin.tar.gz"
+    sha256 "ca09201886bd12c92dd1646662693966cb783b9577105b34acabeb1b9f5a8b50"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tombruijn/lintje/releases/download/v0.6.1/x86_64-apple-darwin.tar.gz"
-    sha256 "4c9f5bde3b7799491ebe5c4cc3499e8a3c8e704a623c9bc0e96a92e1fc5403d4"
+    url "https://github.com/tombruijn/lintje/releases/download/v0.7.0/x86_64-apple-darwin.tar.gz"
+    sha256 "3f17f1c8b3adef164d4c1814585f382827531310de0b076a6b928972ee548a70"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/tombruijn/lintje/releases/download/v0.6.1/aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "7970eec24f10c2700abed9ad8bf3da7f5894307fac2158ced35a8de1a0373c2f"
+    url "https://github.com/tombruijn/lintje/releases/download/v0.7.0/aarch64-unknown-linux-gnu.tar.gz"
+    sha256 "d3eece81637492cf588336ad4ca95328c64a8c941a2b0fa2efb22fe5a9cff54c"
   else
-    url "https://github.com/tombruijn/lintje/releases/download/v0.6.1/x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "78d99329fd4c4e7a739db8a19104d9e079bc07486f1718c1c3acb50c58ecb48c"
+    url "https://github.com/tombruijn/lintje/releases/download/v0.7.0/x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "263583ac6abdcb098493a9dc399aed36b6e83f62b56b3065aac340cef832b76a"
   end
 
   def install
