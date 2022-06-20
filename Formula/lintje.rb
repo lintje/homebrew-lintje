3011bc125a9e4bcbafde719b52e34de1571ed646
Tom de Bruijn <tom@tomdebruijn.com>
2022-06-20 17:43:31 +0200

    Add v0.7.1 release
    Bump the tap to use the latest version for new installs and
    update existing installs.

--- Formula/lintje.rb
+++ Formula/lintje.rb
@@ -1,20 +1,20 @@
 class Lintje < Formula
   desc "Opinionated linter for Git"
   homepage "https://lintje.dev"
-  version "0.7.0"
+  version "0.7.1"
   license "MIT"
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tombruijn/lintje/releases/download/v0.7.0/aarch64-apple-darwin.tar.gz"
-    sha256 "ca09201886bd12c92dd1646662693966cb783b9577105b34acabeb1b9f5a8b50"
+    url "https://github.com/tombruijn/lintje/releases/download/v0.7.1/aarch64-apple-darwin.tar.gz"
+    sha256 "8ebdff786daaacfc749114d70d90f36489f02d72d2670ab5882dfa3c3525c917"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tombruijn/lintje/releases/download/v0.7.0/x86_64-apple-darwin.tar.gz"
-    sha256 "3f17f1c8b3adef164d4c1814585f382827531310de0b076a6b928972ee548a70"
+    url "https://github.com/tombruijn/lintje/releases/download/v0.7.1/x86_64-apple-darwin.tar.gz"
+    sha256 "0cd94e8ac9453b643631601e8f1369494a050c265b9d7165710fd03613513129"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/tombruijn/lintje/releases/download/v0.7.0/aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "d3eece81637492cf588336ad4ca95328c64a8c941a2b0fa2efb22fe5a9cff54c"
+    url "https://github.com/tombruijn/lintje/releases/download/v0.7.1/aarch64-unknown-linux-gnu.tar.gz"
+    sha256 "3ef0bcb1bb4d5df45dd4e4e9f48005415415577b8ec38e13e22f5ef100774a2d"
   else
-    url "https://github.com/tombruijn/lintje/releases/download/v0.7.0/x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "263583ac6abdcb098493a9dc399aed36b6e83f62b56b3065aac340cef832b76a"
+    url "https://github.com/tombruijn/lintje/releases/download/v0.7.1/x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "992046aa86318ac889e06431d8e1f23befb6e214b36dc82bb54e2797b55c89e8"
   end
 
   def install
