4bbe2c6b65e7138a744d0c73aa828ceda58001eb
Tom de Bruijn <tom@tomdebruijn.com>
2022-08-01 19:00:09 +0200

    Add v0.8.0 release
    Bump the tap to use the latest version for new installs and
    update existing installs.
    
    Update the repository location to its new place in the Lintje GitHub
    organization.

--- Formula/lintje.rb
+++ Formula/lintje.rb
@@ -1,20 +1,20 @@
 class Lintje < Formula
   desc "Opinionated linter for Git"
   homepage "https://lintje.dev"
-  version "0.7.1"
+  version "0.8.0"
   license "MIT"
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tombruijn/lintje/releases/download/v0.7.1/aarch64-apple-darwin.tar.gz"
-    sha256 "8ebdff786daaacfc749114d70d90f36489f02d72d2670ab5882dfa3c3525c917"
+    url "https://github.com/lintje/lintje/releases/download/v0.8.0/aarch64-apple-darwin.tar.gz"
+    sha256 "feb1ff3d5829d987d3bbc2f71af7f4a94246bc59bfc97619f1e6f44c860d9260"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tombruijn/lintje/releases/download/v0.7.1/x86_64-apple-darwin.tar.gz"
-    sha256 "0cd94e8ac9453b643631601e8f1369494a050c265b9d7165710fd03613513129"
+    url "https://github.com/lintje/lintje/releases/download/v0.8.0/x86_64-apple-darwin.tar.gz"
+    sha256 "c934da2cb45275070b474bb8f88e7631ea2fbacb10429842b8359087ccf970f5"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/tombruijn/lintje/releases/download/v0.7.1/aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "3ef0bcb1bb4d5df45dd4e4e9f48005415415577b8ec38e13e22f5ef100774a2d"
+    url "https://github.com/tombruijn/lintje/releases/download/v0.8.0/aarch64-unknown-linux-gnu.tar.gz"
+    sha256 "4113d36da30a9ef493a65311b7907e1249c5cc8231596dc96541f11f1a86cde7"
   else
-    url "https://github.com/tombruijn/lintje/releases/download/v0.7.1/x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "992046aa86318ac889e06431d8e1f23befb6e214b36dc82bb54e2797b55c89e8"
+    url "https://github.com/lintje/lintje/releases/download/v0.8.0/x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "c93ad073bf0685672b2f0df750c3a268fd16e1d1e3540bafdc52df5c8b7474bb"
   end
 
   def install
