57c93422ab19bd4c904e464ea561558bd5303174
Tom de Bruijn <tom@tomdebruijn.com>
2023-01-28 15:25:59 +0100

    Add v0.11.3 release
    Bump the tap to use the latest version for new installs and
    update existing installs.

--- Formula/lintje.rb
+++ Formula/lintje.rb
@@ -1,21 +1,21 @@
 class Lintje < Formula
   desc "Opinionated linter for Git"
   homepage "https://lintje.dev"
-  package_version = "v0.11.2"
+  package_version = "v0.11.3"
   version package_version
   license "MIT"
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/lintje/lintje/releases/download/#{package_version}/aarch64-apple-darwin.tar.gz"
-    sha256 "5760f9a120c468f32c286317f38e6eb843fc19077e931fd4c7ad3e3675177281"
+    sha256 "4129c1d4c236dd1a318ec699c68cb6911b6bdc561651ca1fa356d122923f4fb5"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/lintje/lintje/releases/download/#{package_version}/x86_64-apple-darwin.tar.gz"
-    sha256 "f97e12958ce22e323678c4c517b0d736be0d6e86416cf5130c77bb0d6820644e"
+    sha256 "92b4bb385ff5ab691ee58d7203ba15284b93e22b23a584cfda7013c6a3d3b7b8"
   elsif OS.linux? && Hardware::CPU.arm?
     url "https://github.com/tombruijn/lintje/releases/download/#{package_version}/aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "d304e8779ddd8d2ed0be11c6a8cafbb225f6d1f128bd6125fd3ab8efda0fd678"
+    sha256 "2b1265b9c26e527950418b39f077036398fc8bb22cde63a8e9395d423e57ddf5"
   else
     url "https://github.com/lintje/lintje/releases/download/#{package_version}/x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "b4f9af043283ad39a9e2c25105fafe65db2df5abe497145caffb9e05ab28445c"
+    sha256 "052e81d54afa0b91e9cbf86c1e40540379632353637756b7f7b03e920810e636"
   end
 
   def install
